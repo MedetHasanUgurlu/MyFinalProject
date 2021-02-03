8491e9026a1dbec0af029dc768e2eb8b9d9e5ec6
MedetHasanUgurlu <77449083+MedetHasanUgurlu@users.noreply.github.com>
2021-02-03 23:54:32 +0300

    Add files via upload

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        ////Bu yeterli değil
+        //InMemoryProductDal ınMemoryProductDal;
+
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public InMemoryProductDal InMemoryProductDal { get; }
+
+        public List<Product> GetAll()
+        {
+            //İş Kodları
+            //Yetkisi var mı?
+
+            return _productDal.GetAll();
+
+        }
+    }
+}
